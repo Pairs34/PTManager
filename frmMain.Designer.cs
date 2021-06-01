696fb914c0e519232cd0a6852b085856bc7501c1
Pairs34 <34889012+Pairs34@users.noreply.github.com>
2021-06-01 22:47:40 +0300

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace PTManager
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartAll = new System.Windows.Forms.Button();
+            this.btnClearDB = new System.Windows.Forms.Button();
+            this.lstApps = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStartAll
+            // 
+            this.btnStartAll.Location = new System.Drawing.Point(12, 12);
+            this.btnStartAll.Name = "btnStartAll";
+            this.btnStartAll.Size = new System.Drawing.Size(157, 23);
+            this.btnStartAll.TabIndex = 0;
+            this.btnStartAll.Text = "Tümünü Başlat";
+            this.btnStartAll.UseVisualStyleBackColor = true;
+            this.btnStartAll.Click += new System.EventHandler(this.btnStartAll_Click);
+            // 
+            // btnClearDB
+            // 
+            this.btnClearDB.Location = new System.Drawing.Point(187, 12);
+            this.btnClearDB.Name = "btnClearDB";
+            this.btnClearDB.Size = new System.Drawing.Size(159, 23);
+            this.btnClearDB.TabIndex = 1;
+            this.btnClearDB.Text = "Veritabanını Temizle";
+            this.btnClearDB.UseVisualStyleBackColor = true;
+            this.btnClearDB.Click += new System.EventHandler(this.btnClearDB_Click);
+            // 
+            // lstApps
+            // 
+            this.lstApps.AllowDrop = true;
+            this.lstApps.FormattingEnabled = true;
+            this.lstApps.Location = new System.Drawing.Point(12, 42);
+            this.lstApps.Name = "lstApps";
+            this.lstApps.Size = new System.Drawing.Size(334, 485);
+            this.lstApps.TabIndex = 2;
+            this.lstApps.DragDrop += new System.Windows.Forms.DragEventHandler(this.lstApps_DragDrop);
+            this.lstApps.DragEnter += new System.Windows.Forms.DragEventHandler(this.lstApps_DragEnter);
+            this.lstApps.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lstApps_KeyDown);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 542);
+            this.Controls.Add(this.lstApps);
+            this.Controls.Add(this.btnClearDB);
+            this.Controls.Add(this.btnStartAll);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmMain";
+            this.Text = "Price Tracker Starter";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartAll;
+        private System.Windows.Forms.Button btnClearDB;
+        private System.Windows.Forms.ListBox lstApps;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PTManager
+{
+    public partial class frmMain : Form
+    {
+        private string SETTINGS_PATH = Application.StartupPath + "\\settings.ini";
+        public frmMain()
+        {
+            InitializeComponent();
+
+            if (!File.Exists(SETTINGS_PATH))
+            {
+                File.Create(SETTINGS_PATH).Close();
+            }
+        }
+
+        private void lstApps_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void lstApps_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, true);
+                AddToList(s.FirstOrDefault());
+            }
+        }
+
+        void AddToList(string filePath)
+        {
+            lstApps.Items.Add(filePath);
+            SaveToIniFile(filePath);
+        }
+
+        void SaveToIniFile(string filePath)
+        {
+            File.AppendAllText(SETTINGS_PATH,filePath + Environment.NewLine);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            lstApps.Items.Clear();
+            if (File.Exists(SETTINGS_PATH))
+            {
+                lstApps.Items.AddRange(File.ReadAllLines(SETTINGS_PATH));
+            }
+        }
+
+        private void btnClearDB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection connection =
+                    new MySqlConnection("Server=localhost;Database=pricetracking;Uid=root;Pwd=toor;");
+                if (connection.State == ConnectionState.Open)
+                {
+                    MySqlCommand truncatePrices = new MySqlCommand("TRUNCATE TABLE prices",connection);
+                    truncatePrices.ExecuteNonQuery();
+
+                    MySqlCommand truncateHistory = new MySqlCommand("TRUNCATE TABLE history", connection);
+                    truncateHistory.ExecuteNonQuery();
+
+                    connection.Close();
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void btnStartAll_Click(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i < lstApps.Items.Count; i++)
+                {
+                    ProcessStartInfo pInfo = new ProcessStartInfo();
+                    pInfo.FileName = lstApps.Items[i].ToString();
+                    pInfo.WorkingDirectory = Path.GetDirectoryName(lstApps.Items[i].ToString());
+                    Process.Start(pInfo);
+                }
+            });
+        }
+
+        private void lstApps_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                if (lstApps.SelectedIndex != -1)
+                {
+                    var lines = File.ReadAllLines(SETTINGS_PATH).ToArray();
+                    File.WriteAllText(SETTINGS_PATH,String.Empty);
+
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        if (lines[i] != lstApps.Items[lstApps.SelectedIndex].ToString())
+                        {
+                            SaveToIniFile(lines[i]);
+                        }
+                    }
+
+                    lstApps.Items.RemoveAt(lstApps.SelectedIndex);
+                    lstApps.Refresh();
+                }
+            }
+        }
+    }
+}
